f07ed3fbb7d17bb6804abf6ffb4e6088a4c30ee1
Z-Nouman <52423718+Z-Nouman@users.noreply.github.com>
2019-07-02 14:42:44 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 var restaurants = ["Chipotle", "Pei Wei", "Wegmans", "Cava Grill", "Chick Fil A", "Dominos", "Mod Pizza", "Potbelly", "Panera", "Pomodoro", "Moby Dick", "Five Guys", "Firehouse Subs", "PF Changs", "Elevation Burger", "Mezeh"];
 var num1;
 var response;
+var again;
+var i;
+function removeElement(arr, value){
+    var newRests = [];
+    for (i=0; i < arr.length; i++){
+        if (i == value){
+            
+        }
+        else{
+            newRests.push(arr[i]);
+        }
+    }
+    return newRests;
+}
 $(document).ready(function () {
     response = document.getElementById("response");
+    again = document.getElementById("Sad");
     num1 = Math.floor((Math.random() * (restaurants.length-1)));
     $("#button").click(function(){
-        response.innerHTML = restaurants[num1];
-        num1 = Math.round((Math.random() * (restaurants.length-1)));
+        again.innerHTML = "Don't like the outcome? Press the button to get a different option!";
+        if (restaurants.length == 0){
+            response.innerHTML = "You ran out of choices, sorry.";
+        }
+        else{
+            response.innerHTML = restaurants[num1];
+            restaurants = removeElement(restaurants, num1);
+            num1 = Math.ceil((Math.random() * (restaurants.length))) - 1;
+            
+        }
     });
 });
